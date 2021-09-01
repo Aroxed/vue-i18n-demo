6052ee70104410a015b519c60cf2cfeb1c521c40
Andrii Petrashenko <petrashenko@gmail.com>
2021-09-01 18:44:16 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloI18n/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloI18n from "./components/HelloI18n";
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    HelloI18n
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <div class="locale-changer">
+    <select v-model="$i18n.locale">
+      <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
+    </select>
+  </div>
+
+  <p>{{ $t('message') }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloI18n',
+  data () {
+    return { langs: ['en', 'uk'] }
+  }
+}
+</script>
+
